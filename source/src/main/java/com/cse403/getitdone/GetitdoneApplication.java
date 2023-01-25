851f913e7fd690950c822bd2551c60242bd3e85a
Sofia Barry <smbarry@cs.washington.edu>
2023-01-24 16:40:56 -0800

    Added basic spring boot project

---
+++ source/src/main/java/com/cse403/getitdone/GetitdoneApplication.java
@@ -0,0 +1,13 @@
+package com.cse403.getitdone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetitdoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetitdoneApplication.class, args);
+    }
+
+}
---
+++ source/src/test/java/com/cse403/getitdone/GetitdoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cse403.getitdone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetitdoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
