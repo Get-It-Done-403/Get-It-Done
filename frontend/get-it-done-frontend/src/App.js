79b87ac05edb71cc74682c0d563fd59aa203877a
Mitchell <mitchly1@gmail.com>
2023-02-01 19:16:36 -0800

    Added React-router and between Home page and calendar page

--- frontend/get-it-done-frontend/src/App.js
+++ frontend/get-it-done-frontend/src/App.js
@@ -1,11 +1,18 @@
-// import './css/App.css';
 import HomePage from "./pages/HomePage";
-
+import CalendarPage from "./pages/CalendarPage";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-      <div>
-        <HomePage/>
-      </div>
+      <Router>
+          <div>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/calendar" element={<CalendarPage/>}/>
+            </Routes>
+          </div>
+      </Router>
   );
 }
 
