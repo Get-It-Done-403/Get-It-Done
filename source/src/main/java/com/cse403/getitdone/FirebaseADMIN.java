5f2544ed4cf0140df3d487f784c7d19fb5ef3e76
aidanpetta <aidanjp8@uw.edu>
2023-02-01 21:49:27 -0800

    more

--- source/src/main/java/com/cse403/getitdone/FirebaseADMIN.java
+++ source/src/main/java/com/cse403/getitdone/FirebaseADMIN.java
@@ -5,20 +5,68 @@ import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.database.*;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FirebaseADMIN {
-
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("server/saving-data/fireblog");
     public static void main(String[] args) throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("./ServiceAccountKey.json");
+        // Initialize Firebase
+        try {
+            // start init
+            FileInputStream serviceAccount = new FileInputStream("./ServiceAccountKey.json");
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+            // Initialize the app with a service account, granting admin privileges
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://get-it-done-7a708-default-rtdb.firebaseio.com")
+                    .build();
+            FirebaseApp.initializeApp(options);
+            Firestore db = FirestoreClient.getFirestore();
+            // end init
 
-        FirebaseApp.initializeApp(options);
-        Firestore db = FirestoreClient.getFirestore();
+            DatabaseReference ref = FirebaseDatabase.getInstance()
+                    .getReference("restricted_access/secret_document");
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Object document = dataSnapshot.getValue();
+                    System.out.println(document);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                }
+            });
+        } catch (IOException err) {
+            System.out.println("ERROR: invalid service account credentials.");
+            System.out.println(err.getMessage());
+            System.exit(1);
+        }
+    }
+
+
+    public void newUser() {
+
+
+    }
+
+    public void getTask() {
+
+    }
+
+    public void deleteTask() {
+
+    }
+
+    public void addCalendar() {
+
+    }
+
+    public void updateCalendar() {
+
     }
 }
