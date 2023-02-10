eb7435b5ac00abca6a422a573c187db0843a71ce
Sofia Barry <smbarry@cs.washington.edu>
2023-02-09 18:39:28 -0800

    Created util class CalendarEntry to represent one hour block in which a Task gets broken down into

--- backend/src/main/java/com/cse403/getitdone/task/Task.java
+++ backend/src/main/java/com/cse403/getitdone/task/Task.java
@@ -2,8 +2,6 @@ package com.cse403.getitdone.task;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 
