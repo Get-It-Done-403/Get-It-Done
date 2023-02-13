024afdb1c4095c19bc09eb0290dc86fc1d775173
Sofia Barry <smbarry@cs.washington.edu>
2023-02-13 09:37:09 -0800

    Removed unsupported endpoints

--- backend/src/main/java/com/cse403/getitdone/calendar/CalendarController.java
+++ backend/src/main/java/com/cse403/getitdone/calendar/CalendarController.java
@@ -4,38 +4,20 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import com.cse403.getitdone.task.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 public class CalendarController {
 
-    private final CalendarService calendarService;
-
-    public CalendarController(CalendarService calendarService) {
-        this.calendarService = calendarService;
-    }
-
-    @GetMapping("/getRemainingTasksToday")
-    public List<Task> getRemainingTasksToday() throws InterruptedException, ExecutionException {
-        return calendarService.getRemainingTasks();
-    }
+    @Autowired
+    CalendarService calendarService;
 
     @GetMapping("/getCompletedTasksToday")
     public List<Task> getCompletedTasksToday() throws InterruptedException, ExecutionException {
         return calendarService.getCompletedTasks();
     }
 
-    @GetMapping("/getThisMonth")
-    public List<Task> getThisMonth() throws InterruptedException, ExecutionException {
-        return calendarService.getThisMonth();
-    }
-
-//    @PostMapping("/allocateTask")
-//    public Calendar allocateTask(@RequestBody Task task) throws InterruptedException, ExecutionException {
-//        return calendarService.allocateTask(task);
-//    }
 }
