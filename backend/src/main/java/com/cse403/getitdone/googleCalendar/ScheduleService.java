9420ad8e43c9eea685f9f6239d546141567253a4
Sofia Barry <smbarry@cs.washington.edu>
2023-02-13 08:53:37 -0800

    Draft for ScheduleTask Service

--- backend/src/main/java/com/cse403/getitdone/googleCalendar/ScheduleService.java
+++ backend/src/main/java/com/cse403/getitdone/googleCalendar/ScheduleService.java
@@ -1,2 +1,21 @@
-package com.cse403.getitdone.googleCalendar;public class ScheduleService {
+package com.cse403.getitdone.googleCalendar;
+
+import com.cse403.getitdone.task.Task;
+import com.cse403.getitdone.utils.CalendarEntry;
+
+import java.util.List;
+
+public class ScheduleService {
+
+    private static List<CalendarEntry> entries;
+
+    public String scheduleTask(final Task task) {
+
+        for(int i = 0; i < task.getHoursToComplete(); i++) {
+            CalendarEntry entry = new CalendarEntry();
+
+        }
+
+        return null;
+    }
 }
