807104bc99bbc92877996349bcb5db9e91663da5
Sofia Barry <smbarry@cs.washington.edu>
2023-02-13 10:49:34 -0800

    Removed unused files

--- backend/src/main/java/com/cse403/getitdone/calendar/CalendarService.java
+++ backend/src/main/java/com/cse403/getitdone/calendar/CalendarService.java
@@ -1,23 +1,11 @@
 package com.cse403.getitdone.calendar;
 
-import com.cse403.getitdone.task.Task;
 import com.cse403.getitdone.utils.CalendarEntry;
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.*;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.internal.NonNull;
-import org.apache.commons.logging.Log;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 @Service
 public class CalendarService {
@@ -25,10 +13,6 @@ public class CalendarService {
     public static final String COL_NAME="users";
     public static final String SUBCOL_NAME="tasks";
     public static final String SUBSUBCOL_NAME="entries";
-
-    //TODO: push to db
-    // user tasks   task1 - details, {entry1, entry2...}
-    //             task2 - details, {entry1, entry2...}
 
     public String addCalendarEntries(final String uid, final String tid, final List<CalendarEntry> entries) {
         Firestore dbFirestore = FirestoreClient.getFirestore();
