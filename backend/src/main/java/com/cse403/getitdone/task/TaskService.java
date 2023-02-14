cbcc303ad45b82002760d5dd344eb763955901d2
Sofia Barry <smbarry@cs.washington.edu>
2023-02-13 18:55:21 -0800

    Added method to delete subcollections of tasks

--- backend/src/main/java/com/cse403/getitdone/task/TaskService.java
+++ backend/src/main/java/com/cse403/getitdone/task/TaskService.java
@@ -11,6 +11,7 @@ import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +22,7 @@ public class TaskService {
 
     public static final String COL_NAME="users";
     public static final String SUBCOL_NAME="tasks";
+    public static final String SUBSUBCOL_NAME="entries";
 
     public String saveTaskDetails(final String uid, final Task task) throws InterruptedException, ExecutionException {
         Firestore dbFirestore = FirestoreClient.getFirestore();
@@ -43,7 +45,7 @@ public class TaskService {
 
         Task task = null;
 
-        if(document.exists()) {
+        if (document.exists()) {
             task = document.toObject(Task.class);
             return task;
         }else {
@@ -57,7 +59,31 @@ public class TaskService {
                 .collection(COL_NAME).document(uid)
                 .collection(SUBCOL_NAME).document(tid)
                 .delete();
+
+        CollectionReference collectionReference = dbFirestore
+                .collection(COL_NAME).document(uid)
+                .collection(SUBCOL_NAME).document(tid)
+                .collection(SUBSUBCOL_NAME);
+
+        deleteCollection(collectionReference, 10);
         return "uid " + uid + ": task with tid " + tid + " has been deleted";
+    }
+
+    public void deleteCollection(CollectionReference collection, int batchSize) {
+        try {
+            ApiFuture<QuerySnapshot> future = collection.limit(batchSize).get();
+            int deleted = 0;
+            List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+            for (QueryDocumentSnapshot document : documents) {
+                document.getReference().delete();
+                ++deleted;
+            }
+            if (deleted >= batchSize) {
+                deleteCollection(collection, batchSize);
+            }
+        } catch (Exception e) {
+            System.err.println("Error deleting collection : " + e.getMessage());
+        }
     }
 
     public List<Task> getTaskList(String uid) throws ExecutionException, InterruptedException {
