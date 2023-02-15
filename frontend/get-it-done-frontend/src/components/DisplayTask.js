85fcac41b3999836cfe2e9a0bb94c3d63634f17b
Mitchell <mitchly1@gmail.com>
2023-02-15 01:27:54 -0800

    Added overdue tasks and add component to display tasks instead of having it hardcoded each time

---
+++ frontend/get-it-done-frontend/src/components/DisplayTask.js
@@ -0,0 +1,35 @@
+import "../css/mainCSS.css";
+import React, {useEffect} from "react";
+
+function DisplayTask(props) {
+
+    return (
+        <div>
+            {props.tasks && props.tasks.map(singleTask => (
+                singleTask.isCompleted === props.completedBool && (singleTask.dueDate.substring(0,10) === props.todayDate.toISOString().substring(0,10)) === props.todayBool
+                && (new Date(props.todayDate.toISOString().substring(0,10)) > new Date(singleTask.dueDate.substring(0,10))) === props.overdueBool ?
+                props.searchTask === "" ?
+
+                <div key={singleTask.tid}>
+                    <button className={"w-full text-left"} onClick={() => props.editTask(singleTask)}>
+                        <li className={"bg-[#FFFFFF] p-3 bg-opacity-40 rounded-[7px] text-[16px] mt-3 mb-3"}>
+                            {singleTask.title}
+                        </li>
+                    </button>
+                </div>
+                : singleTask.title.toLowerCase().includes(props.searchTask.toLowerCase()) ?
+
+                <div key={singleTask.tid}>
+                    <button className={"w-full text-left"} onClick={() => props.editTask(singleTask)}>
+                        <li className={"bg-[#FFFFFF] p-3 bg-opacity-40 rounded-[7px] text-[16px] mt-3 mb-3"}>
+                            {singleTask.title}
+                        </li>
+                    </button>
+                </div>: null
+                :null
+                ))}
+        </div>
+    )
+}
+
+export default DisplayTask;
--- frontend/get-it-done-frontend/src/pages/homePage/HomePage.js
+++ frontend/get-it-done-frontend/src/pages/homePage/HomePage.js
@@ -2,6 +2,7 @@ import "../../css/mainCSS.css";
 import React, {useEffect} from "react";
 import NavBar from "../../components/NavBar";
 import {useState} from "react";
+import DisplayTask from "../../components/DisplayTask";
 
 function HomePage(props) {
     // const dueDate = new Date(props.currentTask.dueDate);
@@ -14,10 +15,11 @@ function HomePage(props) {
     // const date = curr.toISOString()
     //     .substring(0,11).concat(strTime);
 
-    const todayDate = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000) //local Date
-    const todayDateString = todayDate.toISOString().substring(0,10);
+    // const todayDate = new Date()
 
 
+
+    const [todayDate, setTodayDate] = useState(new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000))
     const [tasks, setTasks] = useState([]);
 
     const [searchTask, setSearchTask] = useState("")
@@ -52,88 +54,30 @@ function HomePage(props) {
                     <h1 className={"text-[36px] ml-[12px] border-b-2 border-[#353535]"}> Today's Tasks</h1>
                      <input type={"text"} className={" mt-[12px] text-center border-b-[1px] border-black"} placeholder={"Search for Tasks"} onChange={(e) => setSearchTask(e.target.value)}/>
                      <div className={"tasksContainer"}>
-                        <div className={"p-3 rounded-[7px] mr-8 bg-[#FF3434] bg-opacity-95 text-white text-[28px] flex flex-[.5] list-item flex-col"}>
-                            Remaining Tasks
-                            {tasks && tasks.map(singleTask => (
-                                !singleTask.isCompleted && singleTask.dueDate.substring(0,10) === todayDateString ?
-                                    searchTask === "" ?
-
-                                        <div key={singleTask.tid}>
-                                            <button className={"w-full text-left"} onClick={() => editTask(singleTask)}>
-                                                <li className={"bg-[#FFFFFF] p-3 bg-opacity-40 rounded-[7px] text-[16px] mt-3 mb-3"}>
-                                                    {singleTask.title}
-                                                </li>
-                                            </button>
-                                        </div>
-                                        : singleTask.title.toLowerCase().includes(searchTask.toLowerCase()) ?
-
-                                            <div key={singleTask.tid}>
-                                                <button className={"w-full text-left"} onClick={() => editTask(singleTask)}>
-                                                    <li className={"bg-[#FFFFFF] p-3 bg-opacity-40 rounded-[7px] text-[16px] mt-3 mb-3"}>
-                                                        {singleTask.title}
-                                                    </li>
-                                                </button>
-                                            </div>: null
-                                    :null
-                            ))}
-                            <button className={"w-full bg-[#F2F2F2] mt-3 text-center rounded-[7px] p-3 text-[16px] text-[#251B1B]"} onClick={() => {props.setTrigger("addTask")}}> Create new Task </button>
-
-                        </div>
-                        <div className={"p-3 bg-[#15CD32] rounded-[7px] bg-opacity-95 text-white text-[28px] flex flex-[.5] display-flex flex-col"}>
+                         <div className={"p-3 rounded-[7px] mr-8 bg-[#FF3434] bg-opacity-95 text-white text-[28px] flex flex-[.5] list-item flex-col"}>
+                             Remaining Tasks
+                             <DisplayTask searchTask={searchTask} tasks={tasks} todayDate={todayDate} completedBool={false} editTask={editTask} todayBool={true} overdueBool={false}/>
+                             <button className={"w-full bg-[#F2F2F2] mt-3 text-center rounded-[7px] p-3 text-[16px] text-[#251B1B]"} onClick={() => {props.setTrigger("addTask")}}> Create new Task </button>
+                         </div>
+                         <div className={"p-3 bg-[#15CD32] rounded-[7px] bg-opacity-95 text-white text-[28px] flex flex-[.5] display-flex flex-col"}>
                             Completed Tasks
-
-                            {tasks && tasks.map(singleTask => (
-                                singleTask.isCompleted && singleTask.dueDate.substring(0,10) === todayDateString ?
-                                    searchTask === "" ?
-
-                                        <div key={singleTask.tid}>
-                                            <button className={"w-full text-left"} onClick={() => editTask(singleTask)}>
-                                                <li className={"bg-[#FFFFFF] p-3 bg-opacity-40 rounded-[7px] text-[16px] mt-3 mb-3"}>
-                                                    {singleTask.title}
-                                                </li>
-                                            </button>
-                                        </div>
-                                        : singleTask.title.toLowerCase().includes(searchTask.toLowerCase()) ?
-
-                                            <div key={singleTask.tid}>
-                                                <button className={"w-full text-left"} onClick={() => editTask(singleTask)}>
-                                                    <li className={"bg-[#FFFFFF] p-3 bg-opacity-40 rounded-[7px] text-[16px] mt-3 mb-3"}>
-                                                        {singleTask.title}
-                                                    </li>
-                                                </button>
-                                            </div>: null
-                                    : null
-                            ))}
+                             <DisplayTask searchTask={searchTask} tasks={tasks} todayDate={todayDate} completedBool={true} editTask={editTask} todayBool={true} overdueBool={false}/>
                         </div>
                     </div>
                 </div>
                 <div className={"upcomingTasks"}>
-                    <div className={"text-[20px] w-9/12 mt-[54px] border-b-2 border-[#353535] min-w-[150px] text-center"}> Upcoming Tasks </div>
-                    <div className={"text-[12px]"}>
-                        {/*Estimated Time: {estimatedTime} Hours*/}
-                        {tasks && tasks.map(singleTask => (
-                            (!singleTask.isCompleted && singleTask.dueDate.substring(0,10) !== todayDateString ?
-                                searchTask === "" ?
-                                    <div className={"mt-2"}>
-                                        {/*<button className={"w-full text-left"} onClick={() => editTask(singleTask)}>*/}
-                                        <button className={'w-full text-left'}  onClick={() => editTask(singleTask)}>
-                                            <li className={"bg-[#FFFFFF] p-3 bg-opacity-40 rounded-[7px] text-[12px] mt-3 mb-3"}>
-                                                {singleTask.title}
-                                            </li>
-                                        </button>
-                                        {/*</button>*/}
-                                    </div>
-                                    : singleTask.title.toLowerCase().includes(searchTask.toLowerCase()) ? <div className={"mt-2"}>
-                                        {/*<button className={"w-full text-left"} onClick={() => editTask(singleTask)}>*/}
-                                        <button className={'w-full text-left'}  onClick={() => editTask(singleTask)}>
-                                            <li className={"bg-[#FFFFFF] p-3 bg-opacity-40 rounded-[7px] text-[12px] mt-3 mb-3"}>
-                                                {singleTask.title}
-                                            </li>
-                                        </button>
-                                    </div> : null
-                                : null)
-                        ))}
+                    <div className={"flex-1 mt-[54px]"}>
+                        <div className={"text-[20px] w-9/12 justify-self-start justify-start border-b-2 border-[#353535] min-w-[150px] text-center"}> Upcoming Tasks </div>
+                        <div className={"text-[12px]"}>
+                            <DisplayTask searchTask={searchTask} tasks={tasks} todayDate={todayDate} completedBool={false} editTask={editTask} todayBool={false} overdueBool={false}/>
+                        </div>
                     </div>
+
+                    <div className={"mb-[54px] flex-1 align-end flex-col  flex"}>
+                        <div className={"text-[20px] w-9/12 border-b-2 border-[#353535] min-w-[150px] text-center"}> Overdue Tasks </div>
+                        <DisplayTask searchTask={searchTask} tasks={tasks} todayDate={todayDate} completedBool={false} editTask={editTask} todayBool={false} overdueBool={true}/>
+                    </div>
+
                 </div>
             </div>
         </div>
