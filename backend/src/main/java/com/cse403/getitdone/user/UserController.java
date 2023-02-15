12fbf3bf8d049b1f6d1ace875521bfbc045970dc
aidanpetta <aidanjp8@uw.edu>
2023-02-15 13:05:15 -0800

    username get and add

--- backend/src/main/java/com/cse403/getitdone/user/UserController.java
+++ backend/src/main/java/com/cse403/getitdone/user/UserController.java
@@ -1,4 +1,27 @@
 package com.cse403.getitdone.user;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/getUserName")
+    public String getUserName(@RequestParam String uid) throws ExecutionException, InterruptedException {
+        return userService.getUserName(uid);
+    }
+
+    @PostMapping("/createUserName")
+    public String createUserName(@RequestParam String uid, @RequestParam String userName) throws ExecutionException, InterruptedException {
+        return userService.saveUserName(uid, userName);
+    }
 }
