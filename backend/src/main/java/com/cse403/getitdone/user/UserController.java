bb28b42d884ed746be2d3a57eee1cb15ffab789a
Tye Coleman <68979544+tyejcoleman@users.noreply.github.com>
2023-03-05 20:37:33 -0800

    added endpoint to get google calendar api link

--- backend/src/main/java/com/cse403/getitdone/user/UserController.java
+++ backend/src/main/java/com/cse403/getitdone/user/UserController.java
@@ -1,16 +1,65 @@
 package com.cse403.getitdone.user;
 
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.CalendarScopes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.cse403.getitdone.googleCalendar.GoogleCal;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+
+
 
 @RestController
 public class UserController {
+    private static final NetHttpTransport HTTP_TRANSPORT;
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
+        } catch (GeneralSecurityException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+    /**
+     * Global instance of the JSON factory.
+     */
+    static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    /**
+     * Directory to store authorization tokens for this application.
+     */
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES =
+            Collections.singletonList(CalendarScopes.CALENDAR);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
 
     @Autowired
     UserService userService;
@@ -24,4 +73,31 @@ public class UserController {
     public String createUserName(@RequestParam String uid, @RequestParam String userName) throws ExecutionException, InterruptedException {
         return userService.saveUserName(uid, userName);
     }
+
+    @GetMapping("/auth-url")
+    public String getAuthUrl() throws Exception {
+        InputStream in = GoogleCal.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        // Create the authorization flow
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+
+        // Generate the authorization URL
+        String redirectUri = "http://localhost:8080/auth-callback";
+        String authorizationUrl = flow.newAuthorizationUrl()
+                .setRedirectUri(redirectUri)
+                .build();
+
+        // Return the authorization URL to the frontend
+        return authorizationUrl;
+    }
+
+
 }
